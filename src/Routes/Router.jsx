f77c52ab62912e5841e059e52b5f6923ade7b1d6
appoclayptor <shahreza1414@gmail.com>
2024-12-22 08:46:28 +0600

    added router to the project

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
+import Home from '../Pages/Home';
 
-const Router = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+        ]
+    }
+])
 
-export default Router;
+export default router;
