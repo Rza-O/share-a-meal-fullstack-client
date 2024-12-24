8faa392b7e4b00876fa703dfec6c7ca3c9322ccd
appoclayptor <shahreza1414@gmail.com>
2024-12-24 09:16:28 +0600

    designing in progress for the table

--- src/Pages/ManageMyFood.jsx
+++ src/Pages/ManageMyFood.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteSweep } from 'react-icons/md';
 
 const ManageMyFood = () => {
   return (
     <div className='bg-harvesting bg-no-repeat bg-contain bg-center min-h-screen w-11/12 mx-auto'>
-      <h1>Manage My Food</h1>
+      <div className='text-center space-y-2 mt-5 mb-5'>
+        <h1 className='font-lobster font-bold text-5xl'>Manage Food Donation</h1>
+        <p className='text-sm font-semibold'>Easily track and manage the food items you’ve donated. Keep your contributions organized and make a difference!</p>
+      </div>
+
+
+      {/* Table Starts here */}
       <section class="container px-4 mx-auto">
         <div class="flex flex-col">
           <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -39,16 +47,19 @@ const ManageMyFood = () => {
                       </th>
 
                       <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 ">
-                        Requested By
+                        Location
                       </th>
 
                       <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 F">
                         Quantity
                       </th>
-
-                      <th scope="col" class="relative py-3.5 px-4">
-                        <span class="sr-only">Actions</span>
+                      <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 F">
+                        Actions
                       </th>
+
+                      {/* <th scope="col" class="relative py-3.5 px-4">
+                        <span class="sr-only">Actions</span>
+                      </th> */}
                     </tr>
                   </thead>
                   <tbody class="bg-white divide-y divide-gray-200">
@@ -71,199 +82,30 @@ const ManageMyFood = () => {
                         </div>
                       </td>
                       <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        <div class="flex items-center gap-x-2">
+                        {/* <div class="flex items-center gap-x-2">
                           <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
                           <div>
                             <h2 class="text-sm font-medium text-gray-800 ">Arthur Melo</h2>
                             <p class="text-xs font-normal text-gray-600">authurmelo@example.com</p>
                           </div>
-                        </div>
+                        </div> */}
+                        Mohakhali
                       </td>
                       <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">Monthly subscription</td>
                       <td class="px-4 py-4 text-sm whitespace-nowrap">
                         <div class="flex items-center gap-x-6">
-                          <button class="text-gray-500 transition-colors duration-200  hover:text-indigo-500 focus:outline-none">
-                            Archive
+                          <button class="text-gray-500 transition-colors duration-200  hover:text-primary focus:outline-none">
+                            Update
                           </button>
 
-                          <button class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                            Download
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      {/* <td class="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                        <div class="inline-flex items-center gap-x-3">
-                          <input type="checkbox" class="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
-
-                          <span>#3065</span>
-                        </div>
-                      </td> */}
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700  whitespace-nowrap">
-                        <div class="inline-flex items-center gap-x-3">
-                          {/* <input type="checkbox" class="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
-
-                          <span>#3065</span> */}
-                          <p>Jhal fry</p>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Jan 5, 2022</td>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div class="inline-flex items-center px-3 py-1 text-red-500 rounded-full gap-x-2 bg-red-100/60">
-                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9 3L3 9M3 3L9 9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>
-
-                          <h2 class="text-sm font-normal">Cancelled</h2>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        <div class="flex items-center gap-x-2">
-                          <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
-                          <div>
-                            <h2 class="text-sm font-medium text-gray-800  ">Andi Lane</h2>
-                            <p class="text-xs font-normal text-gray-600 ">andi@example.com</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Monthly subscription</td>
-                      <td class="px-4 py-4 text-sm whitespace-nowrap">
-                        <div class="flex items-center gap-x-6">
-                          <button class="text-gray-500 transition-colors duration-200  hover:text-indigo-500 focus:outline-none">
-                            Archive
-                          </button>
-
-                          <button class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                            Download
+                          <button class="text-red-500 transition-colors duration-200 hover:text-primary focus:outline-none">
+                            Delete
                           </button>
                         </div>
                       </td>
                     </tr>
 
-                    <tr>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700  whitespace-nowrap">
-                        <div class="inline-flex items-center gap-x-3">
-                          <input type="checkbox" class="text-blue-500 border-gray-300 rounded " />
 
-                          <span>#3064</span>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Jan 5, 2022</td>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-emerald-100/60 ">
-                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 3L4.5 8.5L2 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>
-
-                          <h2 class="text-sm font-normal">Paid</h2>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        <div class="flex items-center gap-x-2">
-                          <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80" alt="" />
-                          <div>
-                            <h2 class="text-sm font-medium text-gray-800  ">Kate Morrison</h2>
-                            <p class="text-xs font-normal text-gray-600 ">kate@example.com</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">Monthly subscription</td>
-                      <td class="px-4 py-4 text-sm whitespace-nowrap">
-                        <div class="flex items-center gap-x-6">
-                          <button class="text-gray-500 transition-colors duration-200  hover:text-indigo-500 focus:outline-none">
-                            Archive
-                          </button>
-
-                          <button class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                            Download
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700  whitespace-nowrap">
-                        <div class="inline-flex items-center gap-x-3">
-                          <input type="checkbox" class="text-blue-500 border-gray-300 rounded " />
-
-                          <span>#3063</span>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Jan 4, 2022</td>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-emerald-100/60 ">
-                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 3L4.5 8.5L2 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>
-
-                          <h2 class="text-sm font-normal">Paid</h2>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        <div class="flex items-center gap-x-2">
-                          <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&q=80" alt="" />
-                          <div>
-                            <h2 class="text-sm font-medium text-gray-800  ">Candice Wu</h2>
-                            <p class="text-xs font-normal text-gray-600 ">candice@example.com</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Monthly subscription</td>
-                      <td class="px-4 py-4 text-sm whitespace-nowrap">
-                        <div class="flex items-center gap-x-6">
-                          <button class="text-gray-500 transition-colors duration-200  hover:text-indigo-500 focus:outline-none">
-                            Archive
-                          </button>
-
-                          <button class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                            Download
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700  whitespace-nowrap">
-                        <div class="inline-flex items-center gap-x-3">
-                          <input type="checkbox" class="text-blue-500 border-gray-300 rounded  " />
-
-                          <span>#3062</span>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Jan 4, 2022</td>
-                      <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div class="inline-flex items-center px-3 py-1 text-gray-500 rounded-full gap-x-2 bg-gray-100/60 ">
-                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M4.5 7L2 4.5M2 4.5L4.5 2M2 4.5H8C8.53043 4.5 9.03914 4.71071 9.41421 5.08579C9.78929 5.46086 10 5.96957 10 6.5V10" stroke="#667085" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>
-
-                          <h2 class="text-sm font-normal">Refunded</h2>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        <div class="flex items-center gap-x-2">
-                          <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=644&q=80" alt="" />
-                          <div>
-                            <h2 class="text-sm font-medium text-gray-800 ">Orlando Diggs</h2>
-                            <p class="text-xs font-normal text-gray-600 ">orlando@example.com</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">Monthly subscription</td>
-                      <td class="px-4 py-4 text-sm whitespace-nowrap">
-                        <div class="flex items-center gap-x-6">
-                          <button class="text-gray-500 transition-colors duration-200  hover:text-indigo-500 focus:outline-none">
-                            Archive
-                          </button>
-
-                          <button class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                            Download
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
                   </tbody>
                 </table>
               </div>
@@ -271,7 +113,7 @@ const ManageMyFood = () => {
           </div>
         </div>
 
-        <div class="flex items-center justify-between mt-6">
+        {/* <div class="flex items-center justify-between mt-6">
           <a href="#" class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
@@ -301,10 +143,23 @@ const ManageMyFood = () => {
               <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
             </svg>
           </a>
-        </div>
+        </div> */}
       </section>
     </div>
   );
 };
 
 export default ManageMyFood;
+
+
+{/* 
+    this will give the red design i want 
+  <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+  <div class="inline-flex items-center px-3 py-1 text-red-500 rounded-full gap-x-2 bg-red-100/60">
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 3L3 9M3 3L9 9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+
+    <h2 class="text-sm font-normal">Expired</h2>
+  </div>
+</td> */}
